bbf407e852235fed5fa1aebddb1ae0e7c9263af4
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-09-06 16:27:27 -0300

    Feat: Reformulando design

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
-    $('header button').click(function () {
-        $('form').slideDown();
-    })
-    $('#botao-cancelar').click(function () {
-        $('form').slideUp();
-    })
-    $('form').on('submit', function (event) {
-        event.preventDefault();
+  $("header button").click(function () {
+    $("form").slideDown();
+  });
+  $("#botao-cancelar").click(function () {
+    $("form").slideUp();
+  });
+  $("form").on("submit", function (event) {
+    event.preventDefault();
 
-        const enderecoNovaImagem = $('#endereco-nova-imagem').val();
-        const novoItem = $('<li style="display: none;"></li>');
+    const enderecoNovaImagem = $("#endereco-nova-imagem").val();
+    const novoItem = $('<li style="display: none;" class="image-item"></li>');
 
-        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
-        $(`
+    $(`<img src="${enderecoNovaImagem}" class="image"/>`).appendTo(novoItem);
+    $(`
         <div class="descricao-imagem">
-            <a href="${enderecoNovaImagem}" target="_blank" title="ver imagem em tamanho real">
-            Ver Imagem em Tamanho Real
+            <a href="${enderecoNovaImagem}" target="_blank" title="ver imagem em tamanho real" class="img-link">
+            IMAGEM COMPLETA
             </a>
         </div> 
         `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1000);
-        $("#endereco-nova-imagem").val('');
-
-    })
-})
+    $(novoItem).appendTo("ul");
+    $(novoItem).fadeIn(1000);
+    $("#endereco-nova-imagem").val("");
+  });
+});
